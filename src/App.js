26956be0eaea9eccb7002f9daeb79cfa628ca1e3
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-03-31 22:13:08 -0400

    component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./CurrencyRow";
 
 function App() {
+  const [currencyOptions, setCurrenctyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  console.log("currency options", currencyOptions);
+
+  useEffect(() => {
+    fetch(
+      "https://api.currencyapi.com/v3/latest?apikey=cur_live_9KSbrxVDY2E4v5pLlgFZdcmEVSicfhwCdJjttMxq"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        const firstCurrency = Object.keys(data.data)[0];
+        const secondCurrency = Object.keys(data.data)[1];
+        setCurrenctyOptions([...Object.keys(data.data)]);
+        setFromCurrency(firstCurrency);
+        setToCurrency(secondCurrency);
+      })
+      .catch((error) => {
+        console.log("Fetch error:", error);
+      });
+  }, []);
   return (
     <>
       <h1>Currency Converter</h1>
-      <CurrencyRow />
-      <div>=</div>
-      <CurrencyRow />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectCurrency={fromCurrency}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectCurrency={toCurrency}
+      />
     </>
   );
 }
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+  const { currencyOptions, selectCurrency } = props;
+  console.log("select", selectCurrency);
+
   return (
     <div>
-      <input type="number" />
-      <select>
-        <option value="Hi">Hi</option>
+      <input type="number" className="input" />
+      <select value={selectCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </div>
   );
